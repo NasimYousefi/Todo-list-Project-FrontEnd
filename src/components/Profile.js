b6612ff03f598606e9eef00727b2c1d04c010b94
Maryam Akhzari <maryakhzari@gmail.com>
2024-08-27 10:49:26 +0330

    change import

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from './UserContext';
-import { bottom } from '@popperjs/core';
+
 
 const Profile = () => {
   const navigate = useNavigate();
